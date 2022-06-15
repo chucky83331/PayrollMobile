2a571588af793aab7a1754b9bf15fa763c38650c
chucky83331 <chasmay@shaw.ca>
2022-06-15 13:26:08 -0700

    Formattting Layout and fixes

--- PayrollMobile/MainPage.xaml.cs
+++ PayrollMobile/MainPage.xaml.cs
@@ -11,6 +11,7 @@ namespace PayrollMobile
         }
         string shiftType;
         string shiftTime;
+        string differential;
         string currentItem;
         decimal rate1 = 25.83m;
         decimal rate2 = 38.745m;
@@ -39,21 +40,21 @@ namespace PayrollMobile
                 {
                     WorkDate = Convert.ToDateTime(Title),
 
-                    
+
                     ShiftType = Convert.ToString(picker1.SelectedItem),
                     ShiftTime = Convert.ToString(picker2.SelectedItem),
+                    Diff = Convert.ToDecimal(picker3.SelectedItem),
                     Rate = Convert.ToDecimal(rateEntry.Text),
                     HrsWork = Convert.ToDecimal(hrsWorkEntry.Text),
-                    Diff = Convert.ToDecimal(diffEntry.Text),
-                });
+                }) ; ;
 
 
                 dateHead.Date = DateTime.Now;
                 picker1.SelectedItem = Title;
                 picker2.SelectedItem = Title;
+                picker3.SelectedItem = Title;
                 rateEntry.Text = string.Empty;
                 hrsWorkEntry.Text = string.Empty;
-                diffEntry.Text = string.Empty;
 
                 CollectionView.ItemsSource = await
                     App.Database.GetShiftAsync();
@@ -72,7 +73,8 @@ namespace PayrollMobile
 
             picker1.SelectedItem = lastSelection.ShiftType;
             picker2.SelectedItem = lastSelection.ShiftTime;
-            int P1 = -1; int P2 = -1;
+            picker3.SelectedItem = lastSelection.Diff;
+            int P1 = -1; int P2 = -1; int P3 = -1;
             switch (lastSelection.ShiftType)
             {
                 case "Regular Hours": P1 = 0; break;
@@ -82,6 +84,7 @@ namespace PayrollMobile
                 case "Over Time 2.0": P1 = 4; break;
                 case "Sick Day": P1 = 5; break;
                 case "Vacation Pay": P1 = 6; break;
+                case "Day Off": P1 = 7; break;
             }
             switch (lastSelection.ShiftTime) 
             { 
@@ -89,17 +92,24 @@ namespace PayrollMobile
                 case "Evening": P2 = 1;break;
                 case "Night Shift":P2 = 2;break;
             }
-           
+            switch (lastSelection.Diff)
+            {
+                case 0.00m: P3 = 0; break;
+                case 0.50m: P3 = 1; break;
+                case 0.75m: P3 = 2; break;
+            }
             if (picker1.SelectedIndex != -1) 
             { P1 = Convert.ToInt32(picker1.SelectedIndex); }
             picker1.SelectedIndex = P1;
             if (picker2.SelectedIndex != -1)
             { P2 = Convert.ToInt32(picker2.SelectedIndex); }
             picker2.SelectedIndex = P2;
+            if (picker3.SelectedIndex != -1)
+            { P1 = Convert.ToInt32(picker3.SelectedIndex); }
+            picker3.SelectedIndex = P3;
 
             rateEntry.Text = Convert.ToString(lastSelection.Rate);
             hrsWorkEntry.Text = Convert.ToString(lastSelection.HrsWork);
-            diffEntry.Text = Convert.ToString(lastSelection.Diff);
             totalEntry.Text = Convert.ToString(lastSelection.Total);
         }
         // Update Record after Change
@@ -118,9 +128,12 @@ namespace PayrollMobile
                 {
                     lastSelection.ShiftTime = Convert.ToString(picker2.SelectedItem);
                 }
+                if (Convert.ToString(picker3.SelectedItem) != "Differential")
+                {
+                    lastSelection.Diff = Convert.ToDecimal(picker3.SelectedItem);
+                }
                 lastSelection.Rate = Convert.ToDecimal(rateEntry.Text);
                 lastSelection.HrsWork = Convert.ToDecimal(hrsWorkEntry.Text);
-                lastSelection.Diff = Convert.ToDecimal(diffEntry.Text);
                 //lastSelection.Total = Convert.ToDecimal(totalEntry.Text);
 
                 await App.Database.UpdateShiftAsync(lastSelection);
@@ -133,10 +146,11 @@ namespace PayrollMobile
                 picker1.Title = "Shift Type";
                 picker2.SelectedIndex = -1;
                 picker2.Title = "Shift Time";
+                picker3.SelectedIndex = -1;
+                picker3.Title = "Differential";
 
                 rateEntry.Text = String.Empty;
                 hrsWorkEntry.Text = String.Empty;
-                diffEntry.Text = String.Empty;
                 totalEntry.Text = "0.00";
             }
         }
@@ -152,18 +166,26 @@ namespace PayrollMobile
 
                 dateHead.Date = DateTime.Now;
 
-                shiftType = "Shift Type"; shiftTime = "Shift Time";
+                shiftType = "Shift Type"; shiftTime = "Shift Time"; differential = "Diff";
                 picker1.Title = shiftType;
                 picker1.SelectedItem = shiftType;
                 picker2.Title = shiftTime;
                 picker2.SelectedItem = shiftTime;
+                picker3.Title = differential;
+                picker3.SelectedItem = differential;
                 rateEntry.Text = String.Empty;
                 hrsWorkEntry.Text = String.Empty;
-                diffEntry.Text = String.Empty;
                 totalEntry.Text = "0.00";
             }
         }
-
+        // Sort on Date Column
+        // ===================
+        //private void Sort()
+        //{
+        //    //CollectionView.Sort(CollectionView.ColumnDefinitionsProperty[2],
+        //    CollectionView.(CollectionView.ColumnDefinitionsProperty[2],
+        //        System.ComponentModel.ListSortDirection.Ascending;
+        //}
         private void picker1_Unfocused(object sender, FocusEventArgs e)
         {
             //await Application.Current.MainPage.DisplayAlert("Picker1 Unfocused", $"{picker1.Title}\n {picker1.SelectedItem}\n {picker1.ItemsSource}", "OK");
@@ -182,6 +204,10 @@ namespace PayrollMobile
                 else if (currentItem == "Stat 2.0" || currentItem == "Over Time 2.0")
                 {
                     rateEntry.Text = Convert.ToString(rate3);
+                }
+                else if (currentItem == "Day Off")
+                {
+                    rateEntry.Text = "0.00";
                 }
             }
         }
